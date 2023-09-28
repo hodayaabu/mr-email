11c07976d1135df5407ac4dde7162890280c988a
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2023-09-28 19:41:18 +0300

    change path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mr-email",
+  base: "/mr-email#/emails/inbox",
   plugins: [react()],
 })
